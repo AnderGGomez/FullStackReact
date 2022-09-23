8de828b118411f650b8a2e42e132bd828d66d4e1
Anderson Gomez <anderson.gomez@utp.edu.co>
2022-09-23 17:35:12 -0500

    final version unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -15,25 +15,6 @@ const Static =({text, count})=>{
     </tr>
   )
 }
-/**
-<table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-</table>
- */
 
 const Statitcs = ({ good, neutral, bad }) => {
   if (good + neutral + bad > 0) {
