42458a63cf44f3be770f29b1565cf9904893b714
Anderson Gomez <anderson.gomez@utp.edu.co>
2022-10-13 11:28:54 -0500

    modulo de configuracion

--- part4/list-blogs/index.js
+++ part4/list-blogs/index.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -29,7 +29,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = process.env.PORT
+const PORT = config.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
